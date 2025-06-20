cd1eada9e284997a4eb94071505f20d6eec6dcde
Vasulio <vas_sab07@mail.ru>
2025-06-20 18:26:12 +0700

    edit HT13

--- EXAM/hard_task_13.py
+++ EXAM/hard_task_13.py
@@ -1,14 +1,14 @@
 str1 = "aabcccaaadddffffff"
 str2 = ""
-last = str1[0]
+last_letter = str1[0]
 num = 0
 for i in str1:
-    if last == i:
+    if last_letter == i:
         num += 1
     else:
-        str2 += last + str(num)
+        str2 += last_letter + str(num)
         num = 1
-    last = i
-str2 += last + str(num)
+    last_letter = i
+str2 += last_letter + str(num)
 
 print(str2)
